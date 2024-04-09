a414283583c4f20efebb63d7c98ba4d06cef45c6
Leivzy <leivzy@163.com>
2024-04-09 15:18:42 +0800

    update

---
+++ chapter05/src/main/java/com/itheima/LoginController.java
@@ -0,0 +1,16 @@
+package com.itheima;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Calendar;
+
+public
+@Controller class LoginController {
+    @GetMapping("/toLoginPage")
+    public String toLoginPage(Model model) {
+        model.addAttribute("currentYear", Calendar.getInstance().get(Calendar.YEAR));
+        return "login";
+    }
+}
---
+++ chapter05/src/main/java/com/itheima/config/MyLocalResovel.java
@@ -0,0 +1,40 @@
+package com.itheima.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.Nullable;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.LocaleResolver;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+
+@Configuration
+public class MyLocalResovel implements LocaleResolver {
+
+    @Override
+    public Locale resolveLocale(HttpServletRequest httpServletRequest) {
+        String l = httpServletRequest.getParameter("l");
+        String header = httpServletRequest.getHeader("Accept-Language");
+        Locale locale = null;
+       if (!StringUtils.isEmpty(l)) {
+            String[] split = l.split("_");
+            locale = new Locale(split[0], split[1]);
+        } else {
+            String[] split = header.split(",");
+            String[] split1 = split[0].split("-");
+            locale = new Locale(split1[0], split1[1]);
+        }
+        return locale;
+    }
+
+    @Override
+    public void setLocale(HttpServletRequest httpServletRequset, @Nullable HttpServletResponse httpServletResponse, @Nullable Locale locale) {
+    }
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new MyLocalResovel();
+    }
+}
