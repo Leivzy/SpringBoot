7eaac45913bdab72746e5828824c926dd8881939
Leivzy <leivzy@163.com>
2024-04-11 18:21:01 +0800

    update

---
+++ chapter05/src/main/java/com/itheima/config/MyInterceptor.java
@@ -0,0 +1,34 @@
+package com.itheima.config;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Calendar;
+
+@Component
+public class MyInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String uri = request.getRequestURI();
+        Object loginUser = request.getSession().getAttribute("loginUser");
+        if (uri.startsWith("/admin") && null == loginUser) {
+            response.sendRedirect("/toLoginPage");
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelANdView) throws Exception {
+        request.setAttribute("currentYear", Calendar.getInstance().get(Calendar.YEAR));
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+    }
+}
