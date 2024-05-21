b806984def3ae6995ec7a9ef416bd883999fd3aa
Leivzy <leivzy@163.com>
2024-05-21 19:35:20 +0800

    chatper07-2

--- chapter07/src/main/java/com/itheima/config/SecurityConfig.java
+++ chapter07/src/main/java/com/itheima/config/SecurityConfig.java
@@ -1,16 +1,29 @@
 package com.itheima.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private DataSource dataSource;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        auth.inMemoryAuthentication().passwordEncoder(encoder).withUser("shitou").password(encoder.encode("123456")).roles("common").and().withUser("李四").password(encoder.encode("123456")).roles("vip");
+//        auth.inMemoryAuthentication().passwordEncoder(encoder).withUser("shitou").password(encoder.encode("123456")).roles("common").and().withUser("李四").password(encoder.encode("123456")).roles("vip");
 
+        String userSQL = "select username,password,valid from t_customer " +
+                "where username = ?";
+        String authoritySQL = "select c.username,a.authority from t_customer c," +
+                "t_authority a,t_customer_authority ca where " +
+                "ca.customer_id=c.id and ca.authority_id=a.id and c.username = ?";
+        auth.jdbcAuthentication().passwordEncoder(encoder).dataSource(dataSource)
+                .usersByUsernameQuery(userSQL)
+                .authoritiesByUsernameQuery(authoritySQL);
     }
 }
