3987b77137cde53593e8501e33a6ac468579eb70
Leivzy <leivzy@163.com>
2024-05-21 15:44:56 +0800

    chapter07

---
+++ chapter07/src/main/java/com/itheima/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.itheima.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        auth.inMemoryAuthentication().passwordEncoder(encoder).withUser("shitou").password(encoder.encode("123456")).roles("common").and().withUser("李四").password(encoder.encode("123456")).roles("vip");
+
+    }
+}
---
+++ chapter07/src/test/java/com/itheima/Chapter07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Chapter07ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
