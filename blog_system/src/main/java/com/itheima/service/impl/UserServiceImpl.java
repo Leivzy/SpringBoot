4f87b5cdaafebb92f2a410a29bac165f14257043
Leivzy <leivzy@163.com>
2024-06-03 20:54:51 +0800

    配置国际化/美化login&register

--- blog_system/src/main/java/com/itheima/service/impl/UserServiceImpl.java
+++ blog_system/src/main/java/com/itheima/service/impl/UserServiceImpl.java
@@ -16,24 +16,25 @@ import java.util.Set;
 public class UserServiceImpl implements IUserService {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserRepository userRepository; // 使用 Spring Data JPA 进行数据库操作
 
     @Override
     public boolean existsByUsername(String username) {
-        return userRepository.existsByUsername(username);
+        return userRepository.existsByUsername(username); // 检查用户名是否存在
     }
 
     @Override
     public void save(User user) {
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setCreated(new Date());
-        user.setValid(true);
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(); // 创建密码编码器
+        user.setPassword(passwordEncoder.encode(user.getPassword())); // 对用户密码进行加密
+        user.setCreated(new Date()); // 设置用户创建时间
+        user.setValid(true); // 设置用户有效性
 
+        // 设置用户权限
         Set<UserAuthority> authorities = new HashSet<>();
-        authorities.add(new UserAuthority(user, 2)); // 使用整数值表示权限
+        authorities.add(new UserAuthority(user, 2)); // 使用整数值表示权限，假设 2 表示普通用户
         user.setAuthorities(authorities);
 
-        userRepository.save(user);
+        userRepository.save(user); // 将用户信息保存到数据库
     }
 }
